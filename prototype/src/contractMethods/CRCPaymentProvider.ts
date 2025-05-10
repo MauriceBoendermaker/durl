c99806c53c75fcdc70427e03d84a591443f18997
yassyass2 <yassinabde@outlook.com>
2025-05-10 22:27:14 +0200

    SDK attempt on V1, unsupported method

--- prototype/src/contractMethods/CRCPaymentProvider.ts
+++ prototype/src/contractMethods/CRCPaymentProvider.ts
@@ -1,30 +1,26 @@
 import { sdk } from "utils/CirclesConfig";
 import { ethers } from 'ethers';
+import { Avatar, AvatarInterface } from "@circles-sdk/sdk";
 
-export async function CRCPaymentProvider(sdk: Sdk, recipient: string, amount: string){
+
+export async function CRCPaymentProvider(signer: ethers.Signer,
+  CRC_PAYMENT_AMOUNT: string,
+  CRC_PAYMENT_RECEIVER: string){
+
     try {
 
-    const senderAddress = await sdk.signer.getAddress();
+    const senderAddress = await signer.getAddress();
+    const avatar = await sdk.getAvatar(senderAddress as `0x${string}`);
 
-   
-    const avatar = await sdk.getAvatar(senderAddress as Address);
+    const amount = ethers.parseUnits(CRC_PAYMENT_AMOUNT, 18);
 
-  
-    const tokenContract = avatar.token;
+    const transferTx = await avatar.transfer(
+    CRC_PAYMENT_RECEIVER as `0x${string}`,
+    amount);
 
+    console.log('CRC Transaction confirmed in block ', transferTx.blockNumber);
 
-    const decimals = await tokenContract.decimals();
-    const parsedAmount = ethers.parseUnits(amount, decimals);
-
-
-    const tx = await tokenContract.transfer(recipient, parsedAmount);
-    console.log('Transaction sent:', tx.hash);
-
-
-    const receipt = await tx.wait();
-    console.log('Transaction confirmed in block', receipt.blockNumber);
-
-    return receipt;
+    return transferTx;
 
   } catch (err) {
     console.error('Transfer failed:', err);
